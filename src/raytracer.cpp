80753c63a223a5149a742e028fa965d9d074c5d6
Jan vdK <jan.hvdk@googlemail.com>
2019-07-11 23:37:28 +0100

    added intersection tests

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -12,17 +12,20 @@ const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 
 // viewport parameters
-const unsigned int CANVAS_WIDTH = 800;
-const unsigned int CANVAS_HEIGHT = 450;
-const unsigned int VIEWPORT_WIDTH = 1;
-const unsigned int VIEWPORT_HEIGHT = 1;
-const unsigned int VIEWPORT_DEPTH = 1;
-
 struct Point3
 {
 	float x;
 	float y;
 	float z;
+
+	Point3 operator + (const Point3& rhs) const
+	{
+		return { x + rhs.x, y + rhs.y, z + rhs.z };
+	}
+	Point3 operator - (const Point3& rhs) const
+	{
+		return { x - rhs.x, y - rhs.y, z - rhs.z };
+	}
 };
 
 struct Point2
@@ -31,15 +34,26 @@ struct Point2
 	float y;
 };
 
-const Point3 CanvasToViewport(unsigned int x, unsigned int y)
+const Point2 ASPECT_RATIO = { 16.f, 16.f};
+const unsigned int CANVAS_WIDTH = 960;
+const unsigned int CANVAS_HEIGHT = (CANVAS_WIDTH / ASPECT_RATIO.x) * ASPECT_RATIO.y;
+const unsigned int VIEWPORT_WIDTH = 1;
+const unsigned int VIEWPORT_HEIGHT = 1;
+const unsigned int VIEWPORT_DEPTH = 1;
+
+
+struct Sphere
 {
-	Point3 viewport = { 
-		x * ((float)VIEWPORT_WIDTH / (float)CANVAS_WIDTH),
-		y * ((float)VIEWPORT_HEIGHT / (float)CANVAS_HEIGHT),
-		VIEWPORT_DEPTH
-	};
-	return viewport;
-}
+	Point3 centre;
+	float radius;
+	TGAColor colour;
+};
+
+struct Ray
+{
+	Point3 origin;
+	Point3 direction;
+};
 
 float DistanceSquared(const Point3& lhs, const Point3& rhs)
 {
@@ -51,10 +65,52 @@ float Magnitude(const Point3& vec)
 	return sqrtf(powf(vec.x, 2.f) + powf(vec.y, 2.f) + powf(vec.z, 2.f));
 }
 
+Point3 Normalize(const Point3& vec)
+{
+	float magnitude = Magnitude(vec);
+	return { vec.x / magnitude, vec.y / magnitude, vec.z / magnitude };
+}
+
 float Dot(const Point3& lhs, const Point3& rhs)
 {
 	return (lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z);
 }
+
+bool RaySphereIntersection(const Ray& ray, const Sphere& sphere, std::pair<float, float>& resultOut)
+{
+	Point3 OC = (sphere.centre - ray.origin);
+	Point3 D = (ray.direction);
+
+
+	float k1 = Dot(D, D);
+	float k2 = 2.f * Dot(OC, D);
+	float k3 = Dot(OC, OC) - powf(sphere.radius, 2.f);
+
+	float discriminant = (powf(k2, 2.f) - (4 * k1 * k3));
+	if (discriminant < 0)
+	{
+		return false;
+	}
+
+	float discriminantRoot = sqrtf(discriminant);
+	float divisor = 2.f * k1;
+	resultOut.first  = (-k2 + discriminantRoot) / divisor;
+	resultOut.second = (-k2 - discriminantRoot) / divisor;
+
+	return true;
+}
+
+const Point3 CanvasToViewport(unsigned int x, unsigned int y)
+{
+	Point3 viewport = { 
+		x * ((float)VIEWPORT_WIDTH / (float)CANVAS_WIDTH),
+		y * ((float)VIEWPORT_HEIGHT / (float)CANVAS_HEIGHT),
+		VIEWPORT_DEPTH
+	};
+	return viewport;
+}
+
+
 
 
 int main(int argc, char** argv) {
@@ -62,8 +118,16 @@ int main(int argc, char** argv) {
 	TGAImage image(CANVAS_WIDTH, CANVAS_HEIGHT, TGAImage::RGB);
 
 	float thresholdSqrd = powf(VIEWPORT_HEIGHT / 4.f, 2.f);
+
+	Point3 zeroVec = { 0.f, 0.f, 0.f };
 	Point3 viewportOrigin = { VIEWPORT_WIDTH / 2.f, VIEWPORT_HEIGHT / 2.f, VIEWPORT_DEPTH };
 	Point3 viewportX = { 1.f, 0.f, 0.f };
+
+	// test sphere
+	Sphere redS = { viewportOrigin, 0.25f, red };
+	Ray testRay{ { VIEWPORT_WIDTH / 2.f, VIEWPORT_HEIGHT / 2.f, 0.f }, zeroVec };
+	std::pair<float, float> intersectResult;
+
 	printf("thresholdSqrd: %f\n", thresholdSqrd);
 	TGAColor placeholder = white;
 	for (auto x = 0; x < CANVAS_WIDTH; x++)
@@ -71,16 +135,16 @@ int main(int argc, char** argv) {
 		for (auto y = 0; y < CANVAS_HEIGHT; y++)
 		{
 			Point3 vpPos = CanvasToViewport(x, y);
-			Point3 originToVPPos = { vpPos.x - viewportOrigin .x, vpPos.y - viewportOrigin.y, vpPos.z - viewportOrigin.z };
-			float magnitude = Magnitude(originToVPPos);
-			Point3 oToVPPosNorm = { originToVPPos.x / magnitude, originToVPPos.y / magnitude, originToVPPos.z / magnitude };
+			testRay.direction = vpPos - testRay.origin;
 
-			float dot = Dot(originToVPPos, viewportX);
-			float lerp = (dot + 1.f) / 2.f;
-
-			TGAColor::lerp(red, green, lerp, &placeholder);
-
-			image.set(x, y, placeholder);
+			if (RaySphereIntersection(testRay, redS, intersectResult))
+			{
+				image.set(x, y, redS.colour);
+			}
+			else
+			{
+				image.set(x, y, placeholder);
+			}
 		}
 	}
 
