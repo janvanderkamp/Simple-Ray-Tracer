0f44f2ccea883103d465fd80c6a7f3dbbd0e6ca2
Jan vdK <jan.hvdk@googlemail.com>
2019-07-14 11:33:09 +0100

    adding new vs style project

---
+++ raytracer/src/raytracer.h
@@ -0,0 +1,10 @@
+﻿// raytracer.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+int main(int argc, char** argv);
